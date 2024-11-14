5b4ddeb6ad71a600091dec7a93b7cecc6461d7b0
zhangxurui <zhangxr@autewifi.cn>
2024-11-14 12:50:00 +0800

    修改5

--- app/api/getOrder/[account]/route.js
+++ app/api/getOrder/[account]/route.js
@@ -1,10 +1,11 @@
 import { NextResponse } from 'next/server'
-import pool from '../../../db'
+import { newClient } from '@/utils/supabase/client';
 
 // 注意事项：1、从参数二中解构出来params 2、需要await等待params的状态变为成功
 export async function GET(request,{params}) {
-    console.log('params',params)
-    const id = (await params).account
-    const [rows] = await pool.query(`SELECT * FROM complaint_order where account=${account}`);
-    return NextResponse.json({data:rows})
+    // console.log('params',params)
+    // const id = (await params).account
+    // const [rows] = await pool.query(`SELECT * FROM complaint_order where account=${account}`);
+    // return NextResponse.json({data:rows})
+    return NextResponse.json({name:'zxr'})
 }
