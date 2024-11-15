8752186de9a79738c4e8a8d052ec1c2e39b3a0d2
zhangxurui <zhangxr@autewifi.cn>
2024-11-15 09:05:37 +0800

    修改7

--- app/api/getDeviceAccount/route.js
+++ app/api/getDeviceAccount/route.js
@@ -5,23 +5,10 @@ import { newClient } from '@/utils/supabase/client';
 export async function GET(request) {
     const supabase = await newClient();
 
-    const sql = `
-    SELECT 
-        device_account.deviceAccount,deviceState,pendingOrderNum,
-        SUM(CASE WHEN orderTable.orderState is Null and orderTable.orderType = 1  THEN 1 ELSE 0 END) AS orderState1_count,
-        SUM(CASE WHEN orderTable.orderState is NUll and orderTable.orderType = 2 THEN 1 ELSE 0 END) AS orderState2_count
-    FROM
-        device_account
-    LEFT JOIN
-        orderTable
-    ON
-        device_account.deviceAccount = orderTable.deviceAccount
-    GROUP BY
-        device_account.deviceAccount
-    ORDER BY  
-        orderState1_count DESC,orderState2_count DESC;`
-
-    const result = await supabase.rpc(sql)
+    const result = await supabase.from('device_account')
+        .select(
+            'deviceAccount,deviceState,pendingOrderNum,ordertable(*)',
+        )
     console.log(result)
     return NextResponse.json({data:rows}, { status: 200 })
 }
